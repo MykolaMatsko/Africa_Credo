d409401b18eb111cbed7fe0e873e586a9a989480
Mykola Matsko <mykola.matsko@gmail.com>
2022-11-10 17:26:01 +0200

    cover image from json decoded

---
+++ Africa_Credo/Model/CoverImageModel.swift
@@ -0,0 +1,13 @@
+//
+//  CoverImageModel.swift
+//  Africa_Credo
+//
+//  Created by Mykola Matsko on 10.11.2022.
+//
+
+import SwiftUI
+
+struct CoverImage: Codable, Identifiable {
+    let id: Int
+    let name: String
+}
