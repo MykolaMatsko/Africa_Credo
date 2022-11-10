5913c4d3c375b9b66c8fbe114eed880f5a6af0d5
Mykola Matsko <mykola.matsko@gmail.com>
2022-11-10 15:12:20 +0200

    Initial Commit

---
+++ Africa_Credo/Africa_CredoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Africa_CredoApp.swift
+//  Africa_Credo
+//
+//  Created by Mykola Matsko on 10.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct Africa_CredoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
