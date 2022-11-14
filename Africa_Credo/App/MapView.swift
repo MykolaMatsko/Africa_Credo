40b289856a4cc9b078f2745498925c06f9f7da86
Mykola Matsko <mykola.matsko@gmail.com>
2022-11-14 11:12:44 +0200

    Locations Page added

--- Africa_Credo/App/MapView.swift
+++ Africa_Credo/App/MapView.swift
@@ -34,13 +34,60 @@ struct MapView: View {
             //MapMarker(coordinate: item.location, tint: .accentColor)
             
             //(C) CUSTOM BASIC ANNOTATION (it could be interactive)
+//            MapAnnotation(coordinate: item.location) {
+//                Image("logo")
+//                    .resizable()
+//                    .scaledToFit()
+//                    .frame(width: 32, height: 32, alignment: .center)
+//            }//MapAnnotation
+            
+            //(D) CUSTOM ADVANCED ANNOTATION (it could be interactive)
             MapAnnotation(coordinate: item.location) {
-                Image("logo")
+                MapAnnotationView(location: item)
+            }
+        })//MAP
+        .overlay(
+            HStack{
+                Image("compass")
                     .resizable()
                     .scaledToFit()
-                    .frame(width: 32, height: 32, alignment: .center)
-            }//MapAnnotation
-        })
+                    .frame(width: 48, height: 48, alignment: .center)
+                
+                VStack(alignment: .leading, spacing: 3) {
+                    HStack{
+                        Text("Latitude:")
+                            .font(.footnote)
+                            .fontWeight(.bold)
+                            .foregroundColor(.accentColor)
+                        Spacer()
+                        Text("\(region.center.latitude)")
+                            .font(.footnote)
+                            .foregroundColor(.white)
+                    }//HStack
+                    Divider()
+                    HStack{
+                        Text("Longitude:")
+                            .font(.footnote)
+                            .fontWeight(.bold)
+                            .foregroundColor(.accentColor)
+                        Spacer()
+                        Text("\(region.center.longitude)")
+                            .font(.footnote)
+                            .foregroundColor(.white)
+                    }//HStack
+                }
+            }//HStack
+                .padding(.vertical, 12)
+                .padding(.horizontal, 16)
+                .background(
+                    Color.black
+                        .cornerRadius(8)
+                        .opacity(0.6)
+                )
+                .padding(), alignment: .top
+            
+        )
+        
     }
 }
 
---
+++ Africa_Credo/View/MapAnnotationView.swift
@@ -0,0 +1,49 @@
+//
+//  MapAnnotationView.swift
+//  Africa_Credo
+//
+//  Created by Mykola Matsko on 13.11.2022.
+//
+
+import SwiftUI
+
+struct MapAnnotationView: View {
+    
+    
+    @State private var animation: Double = 0.0
+    var location: NationalParkLocation
+    
+    var body: some View {
+        ZStack {
+            Circle()
+                .fill(Color.accentColor)
+                .frame(width: 54, height: 54, alignment: .center)
+            
+            Circle()
+                .stroke(Color.accentColor)
+                .frame(width: 52, height: 52, alignment: .center)
+                .scaleEffect(1 + CGFloat(animation))
+                .opacity(1 - animation)
+            
+            Image(location.image)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 48, height: 48, alignment: .center)
+            .clipShape(Circle())
+        }//ZStack
+        .onAppear{
+            withAnimation(Animation.easeInOut(duration: 2).repeatForever(autoreverses: false)) {
+                animation = 1
+            }
+        }
+    }
+}
+
+struct MapAnnotationView_Previews: PreviewProvider {
+    static var locations: [NationalParkLocation] = Bundle.main.decode("locations.json")
+    static var previews: some View {
+        MapAnnotationView(location: locations[0])
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
